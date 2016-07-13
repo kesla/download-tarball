d91bc30d032dec5ce926d37602b246571c0b0b1a
David Bjorklund <david.bjorklund@gmail.com>
2016-07-13 20:09:32 +0200

    initial commit

---
+++ example.js
@@ -0,0 +1,38 @@
+import download from 'download-tarball';
+
+download({
+  url: 'http://link-to-tarball/file.tar.gz',
+  dir: '/dir/where/file/will/be/downloaded'
+}).then(() => {
+  console.log('file is now downloaded!');
+}).catch(err => {
+  console.log('oh crap the file could not be downloaded properly');
+  console.log(err);
+});
+
+download({
+  url: 'http://link-to-tarball/file.tar.gz',
+  dir: '/dir/where/file/will/be/downloaded',
+  // custom options that will be forwarded to got.stream(..., opts) can also be set
+  gotOpts: {
+    headers: {
+      beep: 'boop'
+    }
+  }
+}).then(() => {
+  console.log('file is now downloaded!');
+}).catch(err => {
+  console.log('oh crap the file could not be downloaded properly');
+  console.log(err);
+});
+
+download({
+  // neat, tar files works as well!
+  url: 'http://link-to-tarball/file.tar',
+  dir: '/dir/where/file/will/be/downloaded'
+}).then(() => {
+  console.log('file is now downloaded!');
+}).catch(err => {
+  console.log('oh crap the file could not be downloaded properly');
+  console.log(err);
+});
---
+++ test.js
@@ -0,0 +1,44 @@
+import http from 'http';
+import Promise from 'bluebird';
+import {pack} from 'tar-stream';
+import tmp from 'tmp';
+import test from 'tapava';
+import download from './lib';
+import fs from 'then-fs';
+import {join} from 'path';
+
+test('downloadTarball()', function * (t) {
+  const {url, close} = yield new Promise(resolve => {
+    const server = http.createServer((req, res) => {
+      t.match(req.headers, {custom: 'beep-boop'});
+
+      // only testing tar here since we're using gunzip-maybe that handles gzip
+      const packStream = pack();
+      packStream.entry({name: 'test.txt'}, 'this is the test file');
+      packStream.finalize();
+      packStream.pipe(res);
+    }).listen(0, () => {
+      resolve({
+        url: `http://localhost:${server.address().port}/`,
+        close: () => new Promise(resolve => {
+          server.close(() => resolve(null));
+        })
+      });
+    });
+  });
+  const {name: tmpDir} = tmp.dirSync();
+
+  yield download({
+    url,
+    gotOpts: {
+      headers: {
+        custom: 'beep-boop'
+      }
+    },
+    dir: tmpDir
+  });
+  const actualContent = yield fs.readFile(join(tmpDir, 'test.txt'), 'utf8');
+  const expectedContent = 'this is the test file';
+  t.is(actualContent, expectedContent);
+  yield close();
+});
