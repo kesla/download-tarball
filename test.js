376ad51a515460b1d4de25b90a6337407f6abee5
David Bjorklund <david.bjorklund@gmail.com>
2016-07-15 01:17:51 +0200

    use keep-alive

--- test.js
+++ test.js
@@ -1,35 +1,26 @@
-import http from 'http';
-import Promise from 'bluebird';
 import {pack} from 'tar-stream';
 import tmp from 'tmp';
 import test from 'tapava';
 import download from './lib';
 import fs from 'then-fs';
 import {join} from 'path';
+import setupServer from 'http-test-server';
 
 test('downloadTarball()', function * (t) {
-  const {url, close} = yield new Promise(resolve => {
-    const server = http.createServer((req, res) => {
-      t.match(req.headers, {custom: 'beep-boop'});
+  const {baseUrl, shutdown} = yield setupServer((req, res) => {
+    t.match(req.headers, {custom: 'beep-boop'});
 
-      // only testing tar here since we're using gunzip-maybe that handles gzip
-      const packStream = pack();
-      packStream.entry({name: 'test.txt'}, 'this is the test file');
-      packStream.finalize();
-      packStream.pipe(res);
-    }).listen(0, () => {
-      resolve({
-        url: `http://localhost:${server.address().port}/`,
-        close: () => new Promise(resolve => {
-          server.close(() => resolve(null));
-        })
-      });
-    });
+    // only testing tar here since we're using gunzip-maybe that handles gzip
+    const packStream = pack();
+    packStream.entry({name: 'test.txt'}, 'this is the test file');
+    packStream.finalize();
+    packStream.pipe(res);
   });
+
   const {name: tmpDir} = tmp.dirSync();
 
   const d = download({
-    url,
+    url: baseUrl,
     gotOpts: {
       headers: {
         custom: 'beep-boop'
@@ -44,5 +35,5 @@ test('downloadTarball()', function * (t) {
   const actualContent = yield fs.readFile(join(tmpDir, 'test.txt'), 'utf8');
   const expectedContent = 'this is the test file';
   t.is(actualContent, expectedContent);
-  yield close();
+  yield shutdown();
 });
